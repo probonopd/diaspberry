e527ce1876d0800db8e1d1bd3212b0fd757319b7
Lukas Matt <lukas@zauberstuhl.de>
2016-12-05 01:55:16 +0100

    Update meta-diaspberry to latest develop
    Adjusted travis to build for qemuarm and raspberrypi platform
    
    Signed-off-by: Lukas Matt <lukas@zauberstuhl.de>

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-export SSHPASS=$DEPLOY_PASS
-IMAGE=$(find tmp/deploy/images |grep sdimg |head -n1)
-REMOTE=builds/$(date +%s)_diaspberry-${TRAVIS_BRANCH}.img
+if [[ "$MACHINE" -eq "qemuarm" ]]; then
+  # do nothing yet
+else
+  export SSHPASS=$DEPLOY_PASS
+  IMAGE=$(find tmp/deploy/images |grep sdimg |head -n1)
+  REMOTE=builds/$(date +%s)_diaspberry-${TRAVIS_BRANCH}.img
 
-echo "Upload ${IMAGE} to ${REMOTE}"
-sshpass -e scp -oStrictHostKeyChecking=no \
-  $IMAGE $DEPLOY_USER@$DEPLOY_HOST:$REMOTE
+  echo "Upload ${IMAGE} to ${REMOTE}"
+  sshpass -e scp -oStrictHostKeyChecking=no \
+    $IMAGE $DEPLOY_USER@$DEPLOY_HOST:$REMOTE
 
-echo "Update symbolic link on public directory"
-sshpass -e ssh -oStrictHostKeyChecking=no \
-  $DEPLOY_USER@$DEPLOY_HOST "ln -fs ../${REMOTE} builds/latest-${TRAVIS_BRANCH}"
+  echo "Update symbolic link on public directory"
+  sshpass -e ssh -oStrictHostKeyChecking=no \
+    $DEPLOY_USER@$DEPLOY_HOST "ln -fs ../${REMOTE} builds/latest-${TRAVIS_BRANCH}"
+fi
