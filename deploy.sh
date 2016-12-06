1451f3e78e865ae9ec46610e77e23f2dac260e4b
Lukas Matt <lukas@zauberstuhl.de>
2016-12-06 16:29:58 +0100

    Fix deploy script with multiple travis builds

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+export SSHPASS=$DEPLOY_PASS
+IMAGE=$(find tmp/deploy/images |grep sdimg |head -n1)
+REMOTE=builds/$(date +%s)_diaspberry-${TRAVIS_BRANCH}.img
+
 if [[ "$MACHINE" -eq "qemuarm" ]]; then
-  # do nothing yet
-else
-  export SSHPASS=$DEPLOY_PASS
-  IMAGE=$(find tmp/deploy/images |grep sdimg |head -n1)
-  REMOTE=builds/$(date +%s)_diaspberry-${TRAVIS_BRANCH}.img
-
-  echo "Upload ${IMAGE} to ${REMOTE}"
-  sshpass -e scp -oStrictHostKeyChecking=no \
-    $IMAGE $DEPLOY_USER@$DEPLOY_HOST:$REMOTE
-
-  echo "Update symbolic link on public directory"
-  sshpass -e ssh -oStrictHostKeyChecking=no \
-    $DEPLOY_USER@$DEPLOY_HOST "ln -fs ../${REMOTE} builds/latest-${TRAVIS_BRANCH}"
+  IMAGE=$(find tmp/deploy/images |grep '.rootfs.ext4' |head -n1)
+  REMOTE=builds/$(date +%s)_diaspberry-${TRAVIS_BRANCH}.rootfs.ext4
 fi
+
+echo "Upload ${IMAGE} to ${REMOTE}"
+sshpass -e scp -oStrictHostKeyChecking=no \
+  $IMAGE $DEPLOY_USER@$DEPLOY_HOST:$REMOTE
+
+echo "Update symbolic link on public directory"
+sshpass -e ssh -oStrictHostKeyChecking=no \
+  $DEPLOY_USER@$DEPLOY_HOST "ln -fs ../${REMOTE} builds/latest-${TRAVIS_BRANCH}"
